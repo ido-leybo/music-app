620ca8a1992dc33027892fc6ddfe367c0caae020
Ido Leybovitch <idoleybo01@gmail.com>
2021-04-05 20:28:32 +0300

    add css and now the routes work also in the home page

--- src/components/PickBlock.js
+++ src/components/PickBlock.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function PickBlock({ name, src }) {
+export default function PickBlock({ name, src, type, item }) {
+  let id;
+  let category;
+  switch (type) {
+    case "Artists":
+      id = name;
+      category = "artist";
+      break;
+    case "Playlists":
+      id = item.id;
+      category = "playlist";
+      break;
+    case "Albums":
+      id = item.id;
+      category = "album";
+      break;
+    case "Songs":
+      id = item.id;
+      category = "song";
+      break;
+  }
+
   return (
     <li>
-      <img className="pick-img" width="200px" src={src} alt={name} />
-      <h3>{name}</h3>
+      <Link to={`/${category}/${id}`}>
+        <img className="pick-img" width="200px" src={src} alt={name} />
+        <h3>{name}</h3>
+      </Link>
     </li>
   );
 }
--- src/components/TopFive.js
+++ src/components/TopFive.js
@@ -7,7 +7,13 @@ export default function TopFive({ type, list }) {
       <h2>Top 5 {type}</h2>
       <ul className="list-wrapper">
         {list.map((pick, i) => (
-          <PickBlock key={i} src={pick.cover_img} name={pick.name} />
+          <PickBlock
+            key={i}
+            src={pick.cover_img}
+            name={pick.name}
+            item={pick}
+            type={type}
+          />
         ))}
       </ul>
     </div>
