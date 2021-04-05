b3b20a7f9bd29586bd723ae02d1cba82eac7c512
Ido Leybovitch <idoleybo01@gmail.com>
2021-04-05 20:26:56 +0300

    add component to single album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,51 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-export default function Album({ list }) {
+export default function Album({ list, songs }) {
+  const { id } = useParams();
+  const currentAlbum = list.filter((album) => album.id === id);
+  const albumName = currentAlbum[0].name;
+  const artistName = currentAlbum[0].artist;
+  const albumSong = currentAlbum[0].songs;
+  const filterSongs = songs.filter((song) => {
+    const mapArr = albumSong.map((item) => {
+      if (item === song.id) {
+        return item;
+      }
+      return null;
+    });
+    if (mapArr.includes(song.id)) {
+      return song;
+    }
+    return null;
+  });
+
   return (
     <div>
-      <h1>Albums</h1>
+      <h1>
+        <u>{albumName}</u>
+      </h1>
+      <h4>{artistName}</h4>
+      <img
+        width="250px"
+        src={currentAlbum[0].cover_img}
+        alt={currentAlbum[0].name}
+      />
+      <h2>Songs</h2>
       <ul className="list-wrapper">
-        {list.map((item) => {
+        {filterSongs.map((song) => {
           return (
-            <li key={item.name}>
-              <img src={item.cover_img} width="200px" alt={item.name} />
-              <h2>{item.name}</h2>
+            <li key={song.id}>
+              <iframe
+                width="320"
+                height="240"
+                src={song.youtube_link}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              <h4>{song.name}</h4>
             </li>
           );
         })}
--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Album({ list }) {
   return (
-    <div>
+    <div className="albums">
       <h1>Albums</h1>
       <ul className="list-wrapper">
         {list.map((item) => {
           return (
             <li key={item.name}>
-              <img src={item.cover_img} width="200px" alt={item.name} />
-              <h2>{item.name}</h2>
+              <Link to={`/album/${item.id}`}>
+                <img src={item.cover_img} width="200px" alt={item.name} />
+                <h2>{item.name}</h2>
+              </Link>
             </li>
           );
         })}
